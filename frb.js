8cdd5e1edbdd8eaf42744c8286e833fabf1ebc8d
Joel Martinez <joelmartinez@gmail.com>
2013-01-28 23:00:27 -0500

    Added constants, and lerp, toDegrees, and toRadians methods to MathHelper

--- frb.js
+++ frb.js
@@ -1,4 +1,11 @@
 MathHelper = {
+    e: Math.E,
+    log10E: 0.4342945,
+    log2E: 1.442695,
+    pi: Math.PI,
+    piOver2: (Math.PI / 2.0),
+    piOver4: (Math.PI / 4.0),
+    twoPi: (Math.PI * 2.0),
     invert: function (value) {
         return 0 - value;
     },
@@ -6,6 +13,15 @@ MathHelper = {
         if (value < min) return min;
         else if (value > max) return max;
         else return value;
+    },
+    lerp: function(value1, value2, amount) {
+        return value1 + (value2 - value1) * amount;
+    },
+    toDegrees: function(radians) {
+        return (float)(radians * 57.295779513082320876798154814105);
+    },
+    toRadians: function(degrees) {
+        return (float)(degrees * 0.017453292519943295769236907684886);
     }
 };
 
