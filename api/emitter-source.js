ab879a8331a56e69baed941b4c2cf5f5d4210bdf
Joel Martinez <joelmartinez@gmail.com>
2013-04-21 15:50:31 -0400

    The sample editor now dynamically loads the script based on the value of location.hash

---
+++ api/emitter-source.js
@@ -0,0 +1,39 @@
+frb.start({
+    init: function() {
+        window.emitter = new frb.Emitter();
+    },
+    update: function() {
+
+        emitter.position.x = frb.InputManager.mouse.worldX;
+        emitter.position.y = frb.InputManager.mouse.worldY;
+        
+
+        // recycle particles if necessary
+        var list = emitter.items();
+        for (var i = 0; i < list.length; i++) {
+            var particle = list.get(i);
+            if (particle.alpha <= 0) { 
+                emitter.pool.return(particle);
+                continue;
+            }
+
+            particle.alpha -= 0.05;
+        };
+
+        var newParticle = emitter.emitCircle(10);//emitSprite("Frblogo128");
+
+
+        // initialize particle
+
+        var xRange = 100;
+        var gravity = -150.5;
+        var direction = {x: (Math.random() * xRange) - (xRange/2), y: Math.random() * 400};
+
+        newParticle.xVelocity = direction.x;
+        newParticle.yVelocity = direction.y;
+
+        newParticle.yAcceleration = gravity;
+
+        
+    }
+});
---
+++ api/emitter.js
@@ -0,0 +1,4 @@
+{
+	title:"Emitter",
+    description: "This script sets up an emitter to emit circles in random directions. Play with some of the values such as the rate of alpha decay, the size of the circle, or the strength of the gravity."
+}
